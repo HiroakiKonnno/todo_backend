0b869eba71bfd8bb64d3c218d5f3d1e22f5c0ffe
hiro <sample@example.com>
2024-12-29 22:54:44 +0900

    user_idを定義

--- internal/model/task.go
+++ internal/model/task.go
@@ -6,6 +6,7 @@ type Task struct {
 	ID        int      `gorm:"primaryKey;autoIncrement" json:"id"`
 	Title     string    `gorm:"type:varchar(255);not null" json:"title"`
 	Content   string    `gorm:"type:varchar(255)" json:"content"`
+	UserId    int       `gorm:"type:integer;not null" json:"user_id"`
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 }
