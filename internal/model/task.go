6edf6451e6f486afa97d8bf7b4f6c029fcf183aa
hiro <sample@example.com>
2025-01-13 19:50:26 +0900

    期間を追加

--- internal/model/task.go
+++ internal/model/task.go
@@ -7,6 +7,8 @@ type Task struct {
 	Title     string    `gorm:"type:varchar(255);not null" json:"title"`
 	Content   string    `gorm:"type:varchar(255)" json:"content"`
 	UserId    int       `gorm:"type:integer;not null" json:"user_id"`
+	StartDate *time.Time `gorm:"type:date" json:"start_date"`
+	EndDate *time.Time `gorm:"type:date" json:"end_date"`
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 }
